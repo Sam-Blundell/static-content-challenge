40e24ca60384fa6070b3f84f38ed637286f05abf
Sam Blundell <samcalblundell@googlemail.com>
2022-02-13 20:35:43 +0000

    create middleware to read and display file at url indicated location

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const { readFile, } = require('fs/promises');
 
 const app = express();
 
-app.get('/', (req, res) => {
-	res.send('test page');
+app.use((req, res) => {
+	readFile(`./content${req.url}/index.md`)
+		.then((data) => {
+			res.send(data.toString());
+		})
+		.catch((err) => {
+			res.send('404');
+			console.log(err);
+		});
 });
 
 module.exports = app;
