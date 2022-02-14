9e468bc2070971095f7a41f2808f4cd907aa9ea3
Sam Blundell <samcalblundell@googlemail.com>
2022-02-14 19:12:10 +0000

    create test to loop over all valid endpoints and check status code

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -1,16 +1,23 @@
 const { expect } = require('chai');
 const request = require('supertest');
 const app = require('../index.js');
+const getValidEndpoints = require('./testutils.js');
 
-describe('server', () => {
-	describe('/jobs', () => {
-		it('GET:200 sends a get request to the /jobs url', () => {
-			return request(app)
-				.get('/jobs')
-				.expect(200)
-				.then(response => {
-					console.log(response);
-				});
-		});
+const viewsDirectory = 'content';
+
+const validEndpoints = getValidEndpoints(viewsDirectory);
+
+describe('CMS endpoint testing:', () => {
+	describe('Valid endpoint testing:', () => {
+		for (let endPoint of validEndpoints) {
+			it(`GET:200 sends a get request to the ${endPoint} url`, () => {
+				return request(app)
+					.get(endPoint)
+					.expect(200)
+					.then(response => {
+						console.log(response.text);
+					});
+			});
+		}
 	});
 });
