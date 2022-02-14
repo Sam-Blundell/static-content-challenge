7fb14e037b92572f47f07b5ff9c03947fe9a8a92
Sam Blundell <samcalblundell@googlemail.com>
2022-02-14 22:46:28 +0000

    add tests to ensure correct content is being sent on endpoints, import required utility functions

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -1,13 +1,17 @@
-const { expect } = require('chai');
+const { expect, } = require('chai');
 const request = require('supertest');
 const app = require('../index.js');
-const getValidEndpoints = require('./testutils.js');
+const { getValidEndpoints, getMarkdownLookup, } = require('./testutils.js');
+// these are some utility functions necessary for the testing to not
+// rely on hardcoded files and directories within the content folder
 
 const viewsDirectory = 'content';
+const invalidEndpoint = '/CyGbQXdAOADRvJwuailV';
+//It didn't seem worth ensuring that this endpoint was definitely invalid.
 
 const validEndpoints = getValidEndpoints(viewsDirectory);
+const markdownLookupObj = getMarkdownLookup(validEndpoints);
 
-const invalidEndpoint = '/CyGbQXdAOADRvJwuailV';
 
 describe('CMS endpoint testing:', () => {
 	describe('Valid endpoint testing:', () => {
@@ -17,9 +21,21 @@ describe('CMS endpoint testing:', () => {
 					.get(url)
 					.expect(200);
 			});
+			// The tests above and below could easily be combined but it seemed 
+			// worthwhile to separate them to get a better idea of what the 
+			// problem is if they do end up failing.
+			it(`${url} response should match parsed markdown file`, () => {
+				return request(app)
+					.get(url)
+					.then(res => {
+						// looks up main paragraph tag content in the markdown
+						// and checks that the returned html includes it.
+						expect(res.text).to.include(markdownLookupObj[url]);
+					});
+			});
 		}
 	});
-	describe('Invalid endpoint testing', () => {
+	describe('Invalid endpoint testing:', () => {
 		it('STATUS:404 respond with 404 when folder doesn\'t exist', () => {
 			return request(app)
 				.get(invalidEndpoint)
