cb77fad25224e09990eaa91672990a5772abc250
Sam Blundell <samcalblundell@googlemail.com>
2022-02-14 00:24:14 +0000

    add logic to parse and insert markdown to template and serve to approproate urls

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const express = require('express');
 const { readFile, } = require('fs/promises');
 const { marked, } = require('marked');
+const Mustache = require('mustache');
 
 const app = express();
 
-app.use((req, res) => {
-	readFile(`./content${req.url}/index.md`, { encoding: 'utf-8', })
-		.then((data) => {
-			res.send(marked.parse(data));
-		})
+app.get('/*', (req, res) => {
+
+	const mD = readFile(`./content${req.url}/index.md`, { encoding: 'utf-8', });
+	const template = readFile('./template.html', { encoding: 'utf-8', });
+	const allFiles = Promise.all([mD, template,]);
+
+	allFiles.then((data) => {
+		res.send(Mustache.render(data[1], { content: marked.parse(data[0]), }));
+
+	})
 		.catch((err) => {
-			res.send('404');
+			res.status(404).send('404');
 			console.log(err);
 		});
 });
